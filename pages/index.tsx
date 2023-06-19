2e82a25908e04d0f22237e9a216147de904b8a69
user <020200295a@uandina.edu.pe>
2023-06-19 05:33:23 -0700

    aoe

--- pages/index.tsx
+++ pages/index.tsx
@@ -262,7 +262,7 @@ function downloadFile(url:string, fileName:string) {
 
   async function getReporte(){
     try{
-        const ga = await axios.post(DJANGOURL+"/api/reporte/",{
+        const response = await axios.post(DJANGOURL+"/api/reporte/",{
         programa1,
         facultad,
         programa2,
@@ -271,7 +271,19 @@ function downloadFile(url:string, fileName:string) {
       },{
           responseType:'blob'
         })
-      console.log(ga)
+      const href = URL.createObjectURL(response.data);
+
+    // create "a" HTML element with href to file & click
+    const link = document.createElement('a');
+    link.href = href;
+    link.setAttribute('download', 'file.pdf'); //or any other extension
+    document.body.appendChild(link);
+    link.click();
+
+    // clean up "a" element & remove ObjectURL
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+
     }
     catch(err){
 
